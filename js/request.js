412f0f229c19ae68e40e94713cd25b2609098f2c
hjwhuge <hujiawei_h@163.com>
2026-01-22 15:04:56 +0800

    feat: 完成学习感言

--- js/request.js
+++ js/request.js
@@ -1,37 +1,41 @@
-var request={
-	get:function(url,params={}){
-		this.requestBase(url,'get',params)
+var ip = localStorage.getItem('ip')
+var request = {
+	get: function(url, params = {}) {
+		this.requestBase(url, 'get', params)
 	},
-	post:function(url,params={}){
-		this.requestBase(url,'post',params)
+	post: function(url, params = {}) {
+		this.requestBase(url, 'post', params)
 	},
-	put:function(url,params={}){
-		this.requestBase(url,'put',params)
+	put: function(url, params = {}) {
+		this.requestBase(url, 'put', params)
 	},
-	requestBase:function(url,type,params){
-		var headers = {'Content-Type':'application/json'}
+	requestBase: function(url, type, params) {
+		var headers = {
+			'Content-Type': 'application/json'
+		}
 		var token = localStorage.getItem('token')
-		if(token){
-			headers['authorization']=token
+		if (token) {
+			headers['authorization'] = token
 		}
 		// console.log(params)
-		if(params){
-			if(params.data){
-				var strP=Object.keys(params.data).map(key=>key+'='+encodeURIComponent(params.data[key])).join()
-				if('get'==type){
-					params.data=strP
-				}else if('put'==type){
-					url+='?'+strP
-					params.data=''
+		if (params) {
+			if (params.data) {
+				var strP = Object.keys(params.data).map(key => key + '=' + encodeURIComponent(params.data[key]))
+					.join()
+				if (type == 'get') {
+					params.data = strP
 				}
 			}
+			if (params.contentType === false) {
+				delete headers["Content-Type"]
+			}
 		}
-		var p=Object.assign(params,{
-			dataType:'json',
-			type:type,
-			timeout:1000,
-			headers:headers
+		var p = Object.assign(params, {
+			dataType: 'json',
+			type: type,
+			timeout: 1000,
+			headers: headers
 		})
-		mui.ajax(url,p)
+		mui.ajax(url, p)
 	}
 }
